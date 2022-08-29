8c832ef3a0d10c744b97b6d99fd9773cfe358a97
QuqqU <answeqr@gmail.com>
2022-08-30 00:24:47 +0900

    add Doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,22 @@
+//! The powerful function for real-number verification
+
 mod automata;
 
 use automata::FloatingPoint;
 
+/// Verfiy if input string is a real number.
+///
+/// # Arguments
+///
+/// * `string` - A String reference that holds the name of the person
+///
+/// # Example
+///
+/// ```rust
+/// use is_real::is_real;
+/// let float = "-12.34e5".to_string();
+/// assert_eq!(is_real(&float), true);
+/// ```
 pub fn is_real(s: &String) -> bool {
     FloatingPoint::accept(s)
 }
